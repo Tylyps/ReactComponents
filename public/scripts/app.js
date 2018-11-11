f5989860665b1eb948572fe5a0125a211810e857
Tylyps <j.chmielewski@o2.pl>
2018-11-11 15:34:55 +0100

    Ex[;promg JSX and first challenge

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,56 @@ console.log('App.js is runing!');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Hellow React"
+  React.createElement(
+    "h1",
+    null,
+    "Jakub"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "This is some info"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
 );
+
+// Create a templateTwo var JSX expression
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Jakub Ch"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 21"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Location: Poland, Bia\u0142stok"
+  )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
