ba2ce1e7ad7fe944786b873f00231095cd42d221
Tylyps <j.chmielewski@o2.pl>
2018-11-12 14:23:53 +0100

    Changes in counter example

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,31 +1,70 @@
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
+const appRoot = document.getElementById("app");
 
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
 
-const reset = () => {
-  count = 0;
-  renderCounterApp();
-};
+  }
+
+  handleAddOne() {
+    console.log('+1')
+  }
+
+  handleMinusOne() {
+    console.log('-1')
+
+  }
+
+  handleReset() {
+    console.log('reset')
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Count: </h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>Reset</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Counter />, appRoot);
+
+// let count = 0;
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
+// };
+
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+// };
+
+// const reset = () => {
+//   count = 0;
+//   renderCounterApp();
+// };
 
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>reset</button>
+//     </div>
+//   );
 
-  ReactDOM.render(templateTwo, appRoot);
-};
+//   ReactDOM.render(templateTwo, appRoot);
+// };
 
-renderCounterApp();
+// renderCounterApp();
