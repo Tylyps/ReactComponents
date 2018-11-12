2561f27530091f2836ab30cd64a406d478ff5001
Tylyps <j.chmielewski@o2.pl>
2018-11-12 13:35:28 +0100

    Add events, methods and alerts

--- src/App.js
+++ src/App.js
@@ -29,19 +29,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert('handlePick');
+  }
+
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert('Remove All')
+  }
+
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         <ol>
           {this.props.options.map(option => (<Option key={option} optionText={option} />))}
         </ol>
@@ -61,10 +70,24 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      alert(option);
+      e.target.elements.option.value = '';
+    }
+  }
+
   render() {
     return (
       <div>
-        <h2>Add option component</h2>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
       </div>
     );
   }
