d090e03dd6bbd3a3107fe35b4ba866b494f64a3d
Tylyps <j.chmielewski@o2.pl>
2018-11-12 02:04:03 +0100

    Add picking option

--- src/App.js
+++ src/App.js
@@ -18,12 +18,18 @@ const onFormSubmit = (e) => {
     e.target.elements.option.value = '';
     render();
   }
-}
+};
 
 const onRemoveAll = () => {
   app.options = [];
   render();
-}
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
 
 
 const render = () => {
@@ -33,6 +39,7 @@ const render = () => {
       {app.subtitle && <p>{app.subtitle}</p>}
       <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
       <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should i do?</button>
       <button onClick={onRemoveAll}>Remove All</button>
       {(app.options && app.options.length > 0) && (
       <ol>
